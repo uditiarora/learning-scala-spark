421d345cf9981c87111a198be252824525923bd1
Uditi Arora <uditiarora@gmail.com>
2020-07-30 12:42:42 +0530

    WC2 done

--- src/main/scala/WordCount2_2.scala
+++ src/main/scala/WordCount2_2.scala
@@ -1,3 +1,26 @@
-object WordCount2_2 {
+import org.apache.spark.{SparkConf, SparkContext}
+import util.FileUtil
 
+object WordCount2_2 {
+  def main(args: Array[String]): Unit = {
+
+    val sc = new SparkContext("local", "Word Count (2)", new SparkConf())
+
+    try {
+      val out = "output/kjv-wc2"
+      FileUtil.rmrf(out)
+
+      val input = sc.textFile("data/kjvdat.txt").map(line => line.toLowerCase)
+      val wc = input
+        .flatMap(line => line.split("""[^\p{IsAlpha}]+"""))
+        .map(word => (word, 1))
+        .reduceByKey((count1, count2) => count1 + count2)
+        .sortByKey(ascending =false)
+
+      println(s"Writing output to: $out")
+      wc.saveAsTextFile(out)
+    } finally {
+      sc.stop()
+    }
+  }
 }
